8c41e1633aba131b460682805a8c8ae714dc5c50
elprogramadordeltiempo <elprogramadordeltiempo@outlook.com>
2024-10-09 00:38:26 -0500

    Add ASCII owl project with customizable message

---
+++ owlsay.py
@@ -0,0 +1,13 @@
+# Solicitar al usuario que ingrese un mensaje
+msn = input("Por favor, ingresa un mensaje: ")
+
+# Definir el arte ASCII del búho
+owl_ascii = r"""
+      ,___, °°° """ + msn + r"""
+      [O.o] 
+      /)T_) 
+      --"--"-- 
+"""
+
+# Imprimir el búho
+print(owl_ascii)
